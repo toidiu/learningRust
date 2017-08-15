f089b1dbbd414b4e672c90bd7797aa10122c48e9
toidiu <toidiu@protonmail.com>
2017-08-15 01:00:19 -0400

    revised example

--- adv_lifetimes/src/main.rs
+++ adv_lifetimes/src/main.rs
@@ -1,31 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
 
+fn main() {}
 
-    struct Context<'a>(&'a str);
+struct Context<'s>(&'s str);
 
-    struct Parser<'c, 's: 'c> {
-        context: &'c Context<'s>,
-    }
+struct Parser<'c, 's: 'c> {
+    context: &'c Context<'s>,
+}
 
-    impl<'c, 's: 'c> Parser<'c, 's> {
-        fn parse(&self) -> Result<&'s str, &'s str> {
-//            Ok(&self.context.0[1..])
-            Err(&self.context.0[1..])
-        }
-    }
-
-
-    fn parse_ctx<'a>(context: Context<'a>) -> Result<&'a str, &'a str> {
-        Parser{context: &context}.parse()
-    }
-
-
-
-    let ctx = Context("bla");
-    let res = parse_ctx(ctx);
-    match res {
-        Ok(obj) => println!("success {:?}", obj),
-        Err(er) => println!("fail {}", er),
+impl<'c, 's> Parser<'c, 's> {
+    fn parse<'b>(&'b self) -> Result<(), &'s str> {
+        Err(&self.context.0[1..])
     }
 }
+
+fn parse_context(context: Context) -> Result<(), &str> {
+    Parser { context: &context }.parse()
+    // Ok(())
+}
+
+
